46fa38d1b814622f10ccd47d025a4fa4fc0ac748
leonard <leonard.cs20@sahyadri.edu.in>
2023-06-25 16:20:56 +0530

    android exported true and login and signup page

--- app/src/main/java/com/example/dashbrd/StartupActivity.java
+++ app/src/main/java/com/example/dashbrd/StartupActivity.java
@@ -2,13 +2,40 @@ package com.example.dashbrd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_startup);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button2);
+        Button bu1 = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(StartupActivity.this, SignUpActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+
+
+        });
+        bu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartupActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 }
